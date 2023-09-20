9d320641fb55e37be9e8c3408f407404574faacf
ProfessorShabby <106133748+ProfessorShabby@users.noreply.github.com>
2023-09-19 23:56:16 -0700

    Create HouseSalesAnalysis.py
    Python Code To Analyse Data Sets in King County

---
+++ HouseSalesAnalysis.py
@@ -0,0 +1,18 @@
+# Data Analysis Using Python
+
+### Libraries Used
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import piplite
+await piplite.install(['pandas','matplotlib','scikit-learn','seaborn', 'numpy'])
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler,PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+### Importing DataSets
+
+
+
